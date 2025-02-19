5a8ddb1fe1d67d3f3ffed1916b4ec5a531dc543b
Mauricio Diaz <mauriciodiaz@Mauricios-MacBook-Pro-2.local>
2025-02-19 17:58:44 -0600

    Agregue docstrings a script.py

--- src/script.py
+++ src/script.py
@@ -1,8 +1,20 @@
+"""
+This script runs the whole process of the pipeline
+"""
+
+
 from prep import prep_data
 from train import train_model
 from inference import run_inference
 
 def main():
+    """
+    This function runs the 3 main parts of the pipeline
+    - prep the data
+    - train the model
+    - run inference
+
+    """
     # Preprocess raw data
     prep_data()
 
